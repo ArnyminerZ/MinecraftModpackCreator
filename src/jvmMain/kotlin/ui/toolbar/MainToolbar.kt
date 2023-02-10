8936a41f7b85e0d8d265fa0a3b146865dd85889f
Arnau Mora <arnyminer.z@gmail.com>
2023-02-10 23:46:00 +0100

    Fixed file format
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/jvmMain/kotlin/ui/toolbar/MainToolbar.kt
+++ src/jvmMain/kotlin/ui/toolbar/MainToolbar.kt
@@ -11,7 +11,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ApplicationScope
@@ -32,7 +36,7 @@ fun ApplicationScope.MainToolbar(
     var showProjectPicker by remember { mutableStateOf(false) }
     FilePicker(
         showProjectPicker,
-        fileExtension = "pack.toml",
+        fileExtension = "toml",
     ) { file ->
         file?.let { File(it) }?.let(onProjectPicked)
         showProjectPicker = false
